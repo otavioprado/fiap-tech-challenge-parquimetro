5d767fb8a8d1a1a58d1e14196b511e335fdc295e
otavioprado <otaviofelipedoprado@gmail.com>
2023-09-22 12:31:33 -0300

    Criação do projeto base

---
+++ src/main/java/br/com/fiap/parquimetro/ParquimetroApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.parquimetro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquimetroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParquimetroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/parquimetro/ParquimetroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.parquimetro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquimetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
