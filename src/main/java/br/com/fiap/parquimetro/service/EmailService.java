a6f88cad45729728f390e1e8311c24c1a929903c
renatofilippelli <renatofilippelli@gmail.com>
2023-11-06 22:40:03 -0300

    feat: item-6

--- src/main/java/br/com/fiap/parquimetro/service/EmailService.java
+++ src/main/java/br/com/fiap/parquimetro/service/EmailService.java
@@ -1,10 +1,7 @@
 package br.com.fiap.parquimetro.service;
 
-import br.com.fiap.parquimetro.config.SesService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
--- src/main/java/br/com/fiap/parquimetro/service/SesService.java
+++ src/main/java/br/com/fiap/parquimetro/service/SesService.java
@@ -1,4 +1,4 @@
-package br.com.fiap.parquimetro.config;
+package br.com.fiap.parquimetro.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
