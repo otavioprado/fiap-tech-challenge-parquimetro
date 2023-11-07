951d86c51b9fab2d178d9e6d4e791327832d676f
otavioprado <otaviofelipedoprado@gmail.com>
2023-11-06 23:56:08 -0300

    Refactor: criação da camada FormaPagamentoService

--- src/main/java/br/com/fiap/parquimetro/controller/FormaPagamentoController.java
+++ src/main/java/br/com/fiap/parquimetro/controller/FormaPagamentoController.java
@@ -1,13 +1,9 @@
 package br.com.fiap.parquimetro.controller;
 
 import br.com.fiap.parquimetro.dto.FormaPagamentoDTO;
-import br.com.fiap.parquimetro.mapper.FormaPagamentoMapper;
-import br.com.fiap.parquimetro.model.Condutor;
 import br.com.fiap.parquimetro.model.FormaPagamento;
-import br.com.fiap.parquimetro.repository.CondutorRepository;
-import br.com.fiap.parquimetro.repository.FormaPagamentoRepository;
+import br.com.fiap.parquimetro.service.FormaPagamentoService;
 import lombok.AllArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,35 +15,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
-    private final FormaPagamentoRepository formaPagamentoRepository;
-    private final CondutorRepository condutorRepository;
-    private final FormaPagamentoMapper formaPagamentoMapper;
+    private final FormaPagamentoService formaPagamentoService;
 
     @PostMapping()
     public ResponseEntity<FormaPagamentoDTO> criarFormaPagamento(@RequestBody FormaPagamentoDTO formaPagamentoDTO) {
-        String condutorId = formaPagamentoDTO.getCondutorId();
-        Optional<Condutor> condutorOpt = condutorRepository.findById(condutorId);
-        if (condutorOpt.isEmpty()) {
-            throw new DataIntegrityViolationException("Condutor com ID não encontrado: " + condutorId);
-        }
-
-        FormaPagamento novaFormaPagamento = formaPagamentoRepository.save(formaPagamentoMapper.toEntity(formaPagamentoDTO));
-        Condutor condutor = condutorOpt.get();
-
-        if (condutor.getFormaPagamentos() == null) {
-            condutor.setFormaPagamentos(List.of(novaFormaPagamento));
-        } else {
-            condutor.getFormaPagamentos().add(novaFormaPagamento);
-        }
-
-        condutorRepository.save(condutor);
-
-        return new ResponseEntity<>(formaPagamentoMapper.toDTO(novaFormaPagamento), HttpStatus.CREATED);
+        return new ResponseEntity<>(formaPagamentoService.criarFormaPagamento(formaPagamentoDTO), HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<FormaPagamento> obterFormaPagamento(@PathVariable String id) {
-        Optional<FormaPagamento> formaPagamentoOptional = formaPagamentoRepository.findById(id);
+        Optional<FormaPagamento> formaPagamentoOptional = formaPagamentoService.obterFormaPagamento(id);
 
         if (formaPagamentoOptional.isPresent()) {
             FormaPagamento formaPagamento = formaPagamentoOptional.get();
@@ -58,14 +35,13 @@ public class FormaPagamentoController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<FormaPagamento>> listarFormasPagamento() {
-        List<FormaPagamento> formasPagamento = formaPagamentoRepository.findAll();
-        return new ResponseEntity<>(formasPagamento, HttpStatus.OK);
+    public ResponseEntity<List<FormaPagamentoDTO>> listarFormasPagamento() {
+        return new ResponseEntity<>(formaPagamentoService.findAll(), HttpStatus.OK);
     }
 
     @DeleteMapping()
     public ResponseEntity<Void> deletarTodasFormasDePagamento() {
-        formaPagamentoRepository.deleteAll();
+        formaPagamentoService.deleteAll();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
---
+++ src/main/java/br/com/fiap/parquimetro/service/FormaPagamentoService.java
@@ -0,0 +1,58 @@
+package br.com.fiap.parquimetro.service;
+
+import br.com.fiap.parquimetro.dto.FormaPagamentoDTO;
+import br.com.fiap.parquimetro.mapper.FormaPagamentoMapper;
+import br.com.fiap.parquimetro.model.Condutor;
+import br.com.fiap.parquimetro.model.FormaPagamento;
+import br.com.fiap.parquimetro.repository.CondutorRepository;
+import br.com.fiap.parquimetro.repository.FormaPagamentoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class FormaPagamentoService {
+
+    private final CondutorRepository condutorRepository;
+    private final FormaPagamentoRepository formaPagamentoRepository;
+    private final FormaPagamentoMapper formaPagamentoMapper;
+
+    public FormaPagamentoDTO criarFormaPagamento(FormaPagamentoDTO formaPagamentoDTO) {
+        String condutorId = formaPagamentoDTO.getCondutorId();
+        Optional<Condutor> condutorOpt = condutorRepository.findById(condutorId);
+        if (condutorOpt.isEmpty()) {
+            throw new DataIntegrityViolationException("Condutor com ID não encontrado: " + condutorId);
+        }
+
+        FormaPagamento novaFormaPagamento = formaPagamentoRepository.save(formaPagamentoMapper.toEntity(formaPagamentoDTO));
+        Condutor condutor = condutorOpt.get();
+
+        if (condutor.getFormaPagamentos() == null) {
+            condutor.setFormaPagamentos(List.of(novaFormaPagamento));
+        } else {
+            condutor.getFormaPagamentos().add(novaFormaPagamento);
+        }
+
+        return formaPagamentoMapper.toDTO(novaFormaPagamento);
+    }
+
+    public Optional<FormaPagamento> obterFormaPagamento(String id) {
+        return formaPagamentoRepository.findById(id);
+    }
+
+    public List<FormaPagamentoDTO> findAll() {
+        return formaPagamentoRepository.findAll()
+                .stream()
+                .map(formaPagamentoMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteAll() {
+        formaPagamentoRepository.deleteAll();
+    }
+}
