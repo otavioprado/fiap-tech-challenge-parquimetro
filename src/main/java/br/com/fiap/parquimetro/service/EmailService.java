368be6253720819439428e4fe5a6173cb366f161
otavioprado <otaviofelipedoprado@gmail.com>
2023-11-06 22:20:50 -0300

    Correção: usando SES SDK ao invés de spring-cloud-starter-aws-ses

--- src/main/java/br/com/fiap/parquimetro/service/EmailService.java
+++ src/main/java/br/com/fiap/parquimetro/service/EmailService.java
@@ -1,18 +1,20 @@
 package br.com.fiap.parquimetro.service;
 
+import br.com.fiap.parquimetro.config.SesService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 
 
 @Service @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class EmailService {
 
-    private MailSender mailSender;
+    private SesService sesService;
 
-    public void sendMessage(SimpleMailMessage simpleMailMessage) {
-        this.mailSender.send(simpleMailMessage);
+    public void sendMessage(SendEmailRequest sendEmailRequest) {
+        this.sesService.sendMessage(sendEmailRequest);
     }
 }
